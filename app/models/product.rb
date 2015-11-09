def0d4e98d24dc465957a95a24b65050fd8691e7
Abhishek Darak <abhishek.darak90@gmail.com>
2015-11-09 21:12:15 +0700

    attr_accessor image_file_name

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  attr_accessor :sub_category_id
+  attr_accessor :sub_category_id,:image_file_name 
   attr_accessible :name,:name_url, :product_id,:size,:light,:gurantee,:price, :category_id,:image,:details,:trademak_id,:tag_list, :sub_category_id
   acts_as_taggable
 
