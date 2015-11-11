b440ad0065c6b2c3a7bcb65ca68def9bc851034b
Abhishek Darak <abhishek.darak90@gmail.com>
2015-11-11 17:35:42 +0700

    transition menu, fix top menu scroll

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
 	$("#menu ul.main-menu li.main-menu.home a").addClass("active");
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 180) {
+        $('.header_bottom').css({"position":"fixed","top":"0px","z-index":"1000000000000000000000","width":"82.7%"}, $(window).scrollTop());
+    }else{
+           $('.header_bottom').css({"position":"static","top":"213px","z-index":"1000000000000000000000","width":"100%"}) 
+    }
+  });
 });
