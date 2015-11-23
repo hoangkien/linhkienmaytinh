59074900457dd1bfe0cad9575031424e6c56ffba
hoangkien <abhishek.darak90@gmail.com>
2015-11-23 21:29:28 +0700

    fix menu

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$("#menu ul.main-menu li.main-menu.home a").addClass("active");
   $(window).scroll(function () {
     if ($(window).scrollTop() > 180) {
-        $('.header_bottom').css({"position":"fixed","top":"0px","z-index":"1000000000000000000000","width":"77.7%"}, $(window).scrollTop());
+        $('.header_bottom').css({"position":"fixed","top":"0px","z-index":"1000000000000000000000","width":"1200px"}, $(window).scrollTop());
     }else{
            $('.header_bottom').css({"position":"static","top":"213px","z-index":"1000000000000000000000","width":"100%"})
     }
